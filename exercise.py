75dcf8350a86c9cbe20b98e90481aa027480bb42
raghav1674 <45117311+raghav1674@users.noreply.github.com>
2020-06-24 14:47:07 +0530

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -1,7 +1,7 @@
 import datetime
 import subprocess
 import time
-cmd = ['./exercise.exe']
+cmd = ['exercise.exe']
 while True:
  result = subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode('utf-8')
  datetime_object = datetime.datetime.now()
